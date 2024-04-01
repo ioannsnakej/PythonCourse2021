8b483555053d35c00b26fab6934d66cc21098b89
ioannsnakej <ioannsnakej@gmail.com>
2024-04-01 15:30:59 +0700

    change filter_numbers

--- homework_04/main.py
+++ homework_04/main.py
@@ -11,40 +11,67 @@ def power_numbers_alt(*numbers):
 
 print(power_numbers_alt(1,2,3,4,5,6,7))
 
-ODD = {
-    "ODD": "odd"
-}
+#ODD = {
+#    "ODD": "odd"
+#}
 
-EVEN = {
-    "EVEN": "even"
-}
+#EVEN = {
+#    "EVEN": "even"
+#}
 
-PRIME = {
-    "PRIME": "prime"
-}
+#PRIME = {
+#    "PRIME": "prime"
+#}
+TST = ""
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
 
-def filter_numbers(numbers, **filt):
-    result=[]
-    for key, value in filt.items():
-        if value == "odd":
-            for num in numbers:
-                if (num % 2) != 0:
+# def filter_numbers(numbers, **filt):
+#     result=[]
+#     for key, value in filt.items():
+#         if value == "odd":
+#             for num in numbers:
+#                 if (num % 2) != 0:
+#                     result.append(num)
+#         if value == "even":
+#             for num in numbers:
+#                 if (num % 2) == 0:
+#                     result.append(num)
+#         if value == "prime":
+#             for num in numbers:
+#                 if num > 1:
+#                     for i in range(2, int(num/2)+1):
+#                         if (num % i) == 0:
+#                             break
+#                     else:
+#                         result.append(num)
+#     return result
+
+def filter_numbers(numbers,filt = TST):
+    result = []
+    if filt == ODD:
+        for num in numbers:
+            if (num % 2) != 0:
+                result.append(num)
+    if filt == EVEN:
+        for num in numbers:
+            if (num % 2) == 0:
+                result.append(num)
+    if filt == PRIME:
+        for num in numbers:
+            if num > 1:
+                for i in range(2, int(num/2)+1):
+                    if (num % i) == 0:
+                        break
+                else:
                     result.append(num)
-        if value == "even":
-            for num in numbers:
-                if (num % 2) == 0:
-                    result.append(num)
-        if value == "prime":
-            for num in numbers:
-                if num > 1:
-                    for i in range(2, int(num/2)+1):
-                        if (num % i) == 0:
-                            break
-                    else:
-                        result.append(num)
     return result
 
 list=[1,3,6,-5,7,21,23,8,-4,-8,9,15,13]
-print(filter_numbers(list, **ODD))
-print(filter_numbers(list, **EVEN))
-print(filter_numbers(list, **PRIME))
+# print(filter_numbers(list, **ODD))
+# print(filter_numbers(list, **EVEN))
+# print(filter_numbers(list, **PRIME))
+print(filter_numbers(list, ODD))
+print(filter_numbers(list, EVEN))
+print(filter_numbers(list, PRIME))
