be430d4bb2d0ab96610335c3d2ee2f60b1398a2b
Khodyrev Ivan <Ivan.Khodyrev@evraz.com>
2024-05-06 13:53:41 +0700

    add class BooksDetailsResource, который обрабатывает GET запросы /books/{book_id} и возвращает книгу или сообщение об ошибке

--- homework_09/main.py
+++ homework_09/main.py
@@ -12,10 +12,27 @@ class BooksListResource:
 
         res.text = json.dumps(books_as_list)
 
+class BooksDetailsResource:
+    def on_get(self, req: Request, res: Response, book_id: str):
+        try:
+            book_id = int(book_id)
+            name = BOOKS[book_id]
+        except ValueError:
+            res.status = falcon.HTTP_400
+            result = {"message": f"not valid id {book_id!r}"}
+        except KeyError:
+            res.status = falcon.HTTP_404
+            result = {"message": f"book #{book_id} not found"}
+        else:
+            result = {"id": book_id, "name": name}
+
+        res.text = json.dumps(result)
 
 books_list = BooksListResource()
+book_details = BooksDetailsResource()
 
 app = App()
 
 app.add_route("/books", books_list)
+app.add_route("/books/{book_id}", book_details)
 #/books
