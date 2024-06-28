9560ec17b9e6227f40480540421f0ca3b21caf8e
Khodyrev Ivan <Ivan.Khodyrev@evraz.com>
2024-06-28 16:20:00 +0700

    Добавил домашку 15

---
+++ homework_15/queries.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) UNIQUE,
+    email VARCHAR(80) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE posts
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    body TEXT DEFAULT '',
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+INSERT INTO users (username, email)
+VALUES
+    ('Ivan', 'ivan@example.com'),
+    ('Sofia', 'sofia@example.com');
+
+SELECT * FROM users;
+
+INSERT INTO posts (body, user_id)
+VALUES
+    ('Привет', 1),
+    ('Привет', 2),
+    ('Кодекс обновлен', 1);
+
+SELECT * FROM posts;
